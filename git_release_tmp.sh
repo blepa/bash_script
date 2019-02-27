17373a87246e9fbc5c902de95a87c04cf840afb7
Bartosz Lepa <bartosz.lepa@gmail.com>
2019-02-27 15:57:46 +0100

    Create git_release_tmp.sh

---
+++ git_release_tmp.sh
@@ -0,0 +1,9 @@
+base commit in branch:
+diff -u <(git rev-list --first-parent feature/b003) \
+             <(git rev-list --first-parent develop) | \
+     sed -ne 's/^ //p' | head -1
+	 
+	 
+git diff --name-only develop..feature/EDP-2423 --diff-filter=d
+
+git archive --format=zip HEAD `git diff feature/EDP-2423..develop --name-only` > a.zip
