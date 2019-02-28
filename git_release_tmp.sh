d5abb879de1ddaf5642428dab03a24775e9b7d67
Bartosz Lepa <bartosz.lepa@gmail.com>
2019-02-28 09:49:04 +0100

    Update git_release_tmp.sh

--- git_release_tmp.sh
+++ git_release_tmp.sh
@@ -1,8 +1,9 @@
-base commit in branch:
+#base commit in branch:
 diff -u <(git rev-list --first-parent feature/b003) \
              <(git rev-list --first-parent develop) | \
      sed -ne 's/^ //p' | head -1
-	 
+#one line:
+diff -u <(git rev-list --first-parent feature/b003) <(git rev-list --first-parent develop) | sed -ne 's/^ //p' | head -1	 
 	 
 git diff --name-only develop..feature/EDP-2423 --diff-filter=d
 
