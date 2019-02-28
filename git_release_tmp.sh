3cb3f706b8de6403f36b56ea32378ec7d6d6e717
Bartosz Lepa <bartosz.lepa@gmail.com>
2019-02-28 09:58:05 +0100

    Update git_release_tmp.sh

--- git_release_tmp.sh
+++ git_release_tmp.sh
@@ -3,8 +3,14 @@ diff -u <(git rev-list --first-parent feature/b003) \
              <(git rev-list --first-parent develop) | \
      sed -ne 's/^ //p' | head -1
 #one line:
-diff -u <(git rev-list --first-parent feature/b003) <(git rev-list --first-parent develop) | sed -ne 's/^ //p' | head -1	 
+diff -u <(git rev-list --first-parent feature/b003 --) <(git rev-list --first-parent develop --) | sed -ne 's/^ //p' | head -1	 
 	 
 git diff --name-only develop..feature/EDP-2423 --diff-filter=d
 
 git archive --format=zip HEAD `git diff feature/EDP-2423..develop --name-only` > a.zip
+
+#get list commit
+git log --pretty=format:'%H' -n 1
+
+#get commit count across all branches
+git rev-list --all --count
